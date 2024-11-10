0f41596912c38854796583e25e2d42c700e98d6f
Allysa <nuaysa@gmail.com>
2024-11-10 19:33:29 +0700

    nambal ml

--- challenge-ver14/src/components/products.tsx
+++ challenge-ver14/src/components/products.tsx
@@ -8,62 +8,62 @@ const products: Products[] = [
     {
         Image:"/menu/chocoAlmond.jpg",
         productName: "Almond Chocolate Chips",
-        size:["600", "800"]        
+        size:["600ml", "800ml"]        
     },
     {
         Image:"/menu/vanilla-chocoAlmond.jpg",
         productName: "Vanilla Chocolate Almond",
-        size:["600", "800"]        
+        size:["600ml", "800ml"]        
     },
     {
         Image:"/menu/salut.jpg",
         productName: "Salut Dancow",
-        size:["600", "800"]        
+        size:["600ml", "800ml"]        
     },
     {
         Image:"/menu/nastar.jpg",
         productName: "Nastar",
-        size:["600", "800"]        
+        size:["600ml", "800ml"]        
     },
     {
         Image:"/menu/sagu.jpg",
         productName: "Keju Sagu",
-        size:["600", "800"]        
+        size:["600ml", "800ml"]        
     },
     {
         Image:"/menu/peanut-cookies.jpg",
         productName: "Peanut Cookies",
-        size:["600", "800"]        
+        size:["600ml", "800ml"]        
     },
     {
         Image:"/menu/coklat-stik-keju.jpg",
         productName: "Coklat Stik Keju",
-        size:["600", "800"]        
+        size:["600ml", "800ml"]        
     },
     {
         Image:"/menu/strawberry-cookies.jpg",
         productName: "Thumbrint Strawberry",
-        size:["600", "800"]        
+        size:["600ml", "800ml"]        
     },
     {
         Image:"/menu/lidah-kucing.jpg",
         productName: "Lidah Kucing Keju",
-        size:["600", "800"]        
+        size:["600ml", "800ml"]        
     },
     {
         Image:"/menu/semprit-jadul.jpg",
         productName: "Semprit",
-        size:["600", "800"]        
+        size:["600ml", "800ml"]        
     },
     {
         Image:"/menu/nutella.jpg",
         productName: "Nutella",
-        size:["600", "800"]        
+        size:["600ml", "800ml"]        
     },
     // {
     //     Image:"/menu/",
     //     productName: "Kastengel",
-    //     size:["600", "800"]        
+    //     size:["600ml", "800ml"]        
     // },
     // {
     //     Image:"/menu/",
